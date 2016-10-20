1d34f29cf3d5ddf4f381f13919b53df07d5f4284
Alessio Palmero Aprosio <alessio@apnetwork.it>
2016-10-20 12:02:12 +0200

    JSONization of Tense annotator

--- tint-tense/src/main/java/eu/fbk/dh/tint/tense/TenseAnnotations.java
+++ tint-tense/src/main/java/eu/fbk/dh/tint/tense/TenseAnnotations.java
@@ -2,6 +2,7 @@ package eu.fbk.dh.tint.tense;
 
 import edu.stanford.nlp.ling.CoreAnnotation;
 import edu.stanford.nlp.pipeline.Annotator;
+import eu.fbk.dh.tint.json.JSONLabel;
 
 /**
  * Created by giovannimoretti on 19/05/16.
@@ -11,6 +12,7 @@ public class TenseAnnotations {
     public static final String DH_TENSE = "tense";
     public static final Annotator.Requirement DH_TENSE_REQUIREMENT = new Annotator.Requirement(DH_TENSE);
 
+    @JSONLabel("tense")
     public static class TenseAnnotation implements CoreAnnotation<String> {
 
         public Class<String> getType() {
