ac20881c67d359e3990a8df1ae3779abce11fe46
Alessio Palmero Aprosio <alessio@apnetwork.it>
2020-12-13 22:49:46 +0100

    Tokenization of hours

--- tint-test/src/test/java/TokenizerTest.java
+++ tint-test/src/test/java/TokenizerTest.java
@@ -6,10 +6,9 @@ import eu.fbk.dh.tint.runner.TintPipeline;
 public class TokenizerTest {
 
     public static void main(String[] args) {
-        String text = "Zingaretti ha fondato una S.p.A. Tu no.";
+        String text = "Terri Schiavo, la donna che in questi giorni aveva tenuto col fiato sospeso gli Stati Uniti d'America e causato numerosi dibattiti sul diritto a morire, è morta oggi a St. Petersburg (Florida), alle 9:00 locali, 13 giorni dopo il distacco dai macchinari che la tenevano in vita.";
         TintPipeline pipeline = new TintPipeline();
         pipeline.setProperty("annotators", "ita_toksent");
-        pipeline.setProperty("ita_toksent.ssplitOnlyOnNewLine", "0");
         pipeline.load();
 
         Annotation annotation = pipeline.runRaw(text);
