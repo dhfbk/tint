53fdff9d5c2e226f4d96e62781631e3c342019d6
Alessio Palmero Aprosio <alessio@apnetwork.it>
2021-07-01 18:19:00 +0200

    Bugfix on sequences of abbreviations

--- tint-runner/src/test/java/TokenizerTest.java
+++ tint-runner/src/test/java/TokenizerTest.java
@@ -1,10 +1,8 @@
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.util.CoreMap;
 import eu.fbk.dh.tint.runner.TintPipeline;
-import eu.fbk.dh.tint.runner.TintRunner;
-
-import java.io.IOException;
 
 /**
  * Created by alessio on 04/08/16.
@@ -17,10 +15,13 @@ public class TokenizerTest {
         pipeline.setProperty("annotators", "ita_toksent");
         pipeline.setProperty("ita_toksent.model", "/Users/alessio/Dropbox/relation-extraction/token-settings-wemapp.xml");
         pipeline.setProperty("ita_toksent.newlineIsSentenceBreak", "two");
-        String text = "Prova con due acapo\n\n\n \n\n Ciao\n\nBella lì\nEccoci";
+        String text = "Coll. Prof. Amm.vo Pinco Pallino";
         Annotation annotation = pipeline.runRaw(text);
         for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-            System.out.println(sentence.get(CoreAnnotations.TokensAnnotation.class).size());
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                System.out.println(token);
+            }
+            System.out.println();
         }
     }
 }
