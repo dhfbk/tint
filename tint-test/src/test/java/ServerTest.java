bb93fae3845f69487ee578ed453b024ff04fb54b
Alessio Palmero Aprosio <alessio@apnetwork.it>
2022-01-03 17:15:37 +0100

    Test files

--- tint-test/src/test/java/ServerTest.java
+++ tint-test/src/test/java/ServerTest.java
@@ -6,10 +6,9 @@ public class ServerTest {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("annotators", "ita_toksent, pos, ita_upos, ita_splitter, ita_morpho, ita_lemma, ner, wemapp");
-        properties.setProperty("customAnnotatorClass.wemapp", "edu.stanford.nlp.pipeline.TokensRegexAnnotator");
-        properties.setProperty("wemapp.rules", "example.rules");
-        properties.setProperty("ita_toksent.model", "/Users/alessio/Desktop/token-settings-teresa.xml");
+//        properties.setProperty("annotators", "ita_toksent, pos, ita_upos, ita_splitter, ita_morpho, ita_lemma, ner");
+        properties.setProperty("annotators", "ita_toksent");
+        properties.setProperty("ita_toksent.model", "/Users/alessio/Desktop/DatiMoro/dati2-out/token-settings.xml");
         TintServer server = new TintServer("0.0.0.0", 8012, null, properties);
     }
 }
